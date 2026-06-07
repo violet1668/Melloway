2ef7a611512bfcf55f2171661d384ab102f83059
mage <oceanyi16@163.com>
2026-06-07 20:54:40 +0800

    chore: add deployment config

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 
 from services.route_engine import generate_route_plan
@@ -142,4 +144,6 @@ def _handle_route_generation(flask_request):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", "5000"))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=port, debug=debug)
